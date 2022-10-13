67b9ed4ada2afcdb3d546dfa082deee6060213a2
Abdulsametileri <sametileri07@gmail.com>
2022-10-13 06:59:22 +0300

    refactor: simplfy config value by settings default values

--- internal/config/config.go
+++ internal/config/config.go
@@ -45,6 +45,8 @@ func New(configPath, configName string) (*ApplicationConfig, error) {
 	configuration := ApplicationConfig{}
 
 	v := viper.New()
+	setDefaults(v)
+
 	v.AddConfigPath(configPath)
 	v.SetConfigName(configName)
 
@@ -57,6 +59,28 @@ func New(configPath, configName string) (*ApplicationConfig, error) {
 	}
 
 	return &configuration, nil
+}
+
+func setDefaults(v *viper.Viper) {
+	setKafkaConsumerDefaults(v)
+	setKafkaProducerDefaults(v)
+}
+
+func setKafkaConsumerDefaults(v *viper.Viper) {
+	v.SetDefault("kafka.consumer.minBytes", 10e3)
+	v.SetDefault("kafka.consumer.maxBytes", 10e6)
+	v.SetDefault("kafka.consumer.maxWait", "2s")
+	v.SetDefault("kafka.consumer.commitInterval", "1s")
+	v.SetDefault("kafka.consumer.heartbeatInterval", "3s")
+	v.SetDefault("kafka.consumer.sessionTimeout", "30s")
+	v.SetDefault("kafka.consumer.rebalanceTimeout", "30s")
+	v.SetDefault("kafka.consumer.startOffset", "earliest")
+	v.SetDefault("kafka.consumer.retentionTime", "24h")
+}
+
+func setKafkaProducerDefaults(v *viper.Viper) {
+	v.SetDefault("kafka.producer.batchSize", 100)
+	v.SetDefault("kafka.producer.batchTimeout", "500us")
 }
 
 func (c *ApplicationConfig) Print() {
