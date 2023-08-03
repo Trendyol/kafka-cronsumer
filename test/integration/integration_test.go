d338eb53902a42c229ba3955eae9bebb39742c8a
Abdulsametileri <sametileri07@gmail.com>
2023-08-04 01:58:39 +0300

    chore: add more integration test case

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -3,15 +3,18 @@ package integration
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	cronsumer "github.com/Trendyol/kafka-cronsumer"
 	"github.com/Trendyol/kafka-cronsumer/pkg/kafka"
 	segmentio "github.com/segmentio/kafka-go"
+	"strconv"
 	"testing"
 	"time"
 )
 
 func Test_Should_Consume_Exception_Message_Successfully(t *testing.T) {
+	// Given
 	topic := "exception"
 	_, cleanUp := createTopic(t, topic)
 	defer cleanUp()
@@ -38,14 +41,151 @@ func Test_Should_Consume_Exception_Message_Successfully(t *testing.T) {
 	c := cronsumer.New(config, consumeFn)
 	c.Start()
 
+	// When
 	expectedMessage := kafka.Message{Topic: topic, Value: []byte("some message")}
 	if err := c.Produce(expectedMessage); err != nil {
 		fmt.Println("Produce err", err.Error())
 	}
 
+	// Then
 	actualMessage := <-waitMessageCh
 	if !bytes.Equal(actualMessage.Value, expectedMessage.Value) {
 		t.Errorf("Expected: %s, Actual: %s", expectedMessage.Value, actualMessage.Value)
+	}
+}
+
+func Test_Should_Retry_Message_When_Error_Occurred_During_Consuming(t *testing.T) {
+	// Given
+	topic := "exception-oneRetry"
+	conn, cleanUp := createTopic(t, topic)
+	defer cleanUp()
+
+	config := &kafka.Config{
+		Brokers: []string{"localhost:9092"},
+		Consumer: kafka.ConsumerConfig{
+			GroupID:  "sample-consumer",
+			Topic:    topic,
+			Cron:     "*/1 * * * *",
+			Duration: 20 * time.Second,
+		},
+		LogLevel: "info",
+	}
+
+	errCh := make(chan struct{})
+	var consumeFn kafka.ConsumeFn = func(message kafka.Message) error {
+		fmt.Printf("consumer > Message received: %s\n", string(message.Value))
+		errCh <- struct{}{}
+		return errors.New("err occurred")
+	}
+
+	c := cronsumer.New(config, consumeFn)
+	c.Start()
+
+	// When
+	expectedMessage := kafka.Message{Topic: topic, Value: []byte("some message")}
+	if err := c.Produce(expectedMessage); err != nil {
+		fmt.Println("Produce err", err.Error())
+	}
+
+	// Then
+	<-errCh
+
+	var expectedOffset int64 = 3
+	conditionFunc := func() bool {
+		lastOffset, _ := conn.ReadLastOffset()
+		return lastOffset == expectedOffset
+	}
+
+	assertEventually(t, conditionFunc, 30*time.Second, time.Second)
+}
+
+func Test_Should_Discard_Message_When_Retry_Count_Is_Equal_To_MaxRetrys_Value(t *testing.T) {
+	// Given
+	topic := "exception-max-retry"
+	conn, cleanUp := createTopic(t, topic)
+	defer cleanUp()
+
+	maxRetry := 1
+	config := &kafka.Config{
+		Brokers: []string{"localhost:9092"},
+		Consumer: kafka.ConsumerConfig{
+			GroupID:  "sample-consumer",
+			Topic:    topic,
+			Cron:     "*/1 * * * *",
+			Duration: 20 * time.Second,
+			MaxRetry: maxRetry,
+		},
+		LogLevel: "info",
+	}
+
+	errCh := make(chan struct{})
+	var consumeFn kafka.ConsumeFn = func(message kafka.Message) error {
+		fmt.Printf("consumer > Message received: %s\n", string(message.Value))
+
+		if getRetryCount(message) == maxRetry {
+			errCh <- struct{}{}
+		}
+
+		return errors.New("err occurred")
+	}
+
+	c := cronsumer.New(config, consumeFn)
+	c.Start()
+
+	// When
+	expectedMessage := kafka.Message{Topic: topic, Value: []byte("some message")}
+	if err := c.Produce(expectedMessage); err != nil {
+		fmt.Println("Produce err", err.Error())
+	}
+
+	// Then
+	<-errCh
+
+	var expectedOffset int64 = 3
+	conditionFunc := func() bool {
+		lastOffset, _ := conn.ReadLastOffset()
+		fmt.Println("lastOffset", lastOffset)
+		return lastOffset == expectedOffset
+	}
+
+	assertEventually(t, conditionFunc, 30*time.Second, time.Second)
+}
+
+func getRetryCount(message kafka.Message) int {
+	for _, header := range message.Headers {
+		if header.Key == "x-retry-count" {
+			retryCount, _ := strconv.Atoi(string(header.Value))
+			return retryCount
+		}
+	}
+	return 0
+}
+
+func assertEventually(t *testing.T, condition func() bool, waitFor time.Duration, tick time.Duration) bool {
+	t.Helper()
+
+	ch := make(chan bool, 1)
+
+	timer := time.NewTimer(waitFor)
+	defer timer.Stop()
+
+	ticker := time.NewTicker(tick)
+	defer ticker.Stop()
+
+	for tick := ticker.C; ; {
+		select {
+		case <-timer.C:
+			t.Errorf("Condition never satisfied")
+			return false
+		case <-tick:
+			tick = nil
+			go func() { ch <- condition() }()
+		case v := <-ch:
+			if v {
+				return true
+			}
+			tick = ticker.C
+		}
 	}
 }
 
