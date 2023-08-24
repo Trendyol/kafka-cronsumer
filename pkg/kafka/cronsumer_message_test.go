78785ce39db2c0c52fa9193248c5bd4954a3ce6c
hayrienes.gokdag <hayrienes.gokdag@trendyol.com>
2023-08-24 09:28:40 +0300

    resolve conflicts

--- pkg/kafka/cronsumer_message_test.go
+++ pkg/kafka/cronsumer_message_test.go
@@ -55,78 +55,77 @@ func Test_Should_Build_Message_With_All_Fields(t *testing.T) {
 		}
 	})
 
-
-	func Test_WithTopic(t *testing.T) {
-		//Given
-		var expected = "topic"
-		var messageBuilder = MessageBuilder{}
-		//When
-		actual := messageBuilder.WithTopic(expected).topic
-		//Then
-		if *actual != expected {
-			t.Errorf("Expected: %s, Actual: %s", expected, *actual)
-		}
+}
+func Test_WithTopic(t *testing.T) {
+	//Given
+	var expected = "topic"
+	var messageBuilder = MessageBuilder{}
+	//When
+	actual := messageBuilder.WithTopic(expected).topic
+	//Then
+	if *actual != expected {
+		t.Errorf("Expected: %s, Actual: %s", expected, *actual)
 	}
+}
 
-	func Test_WithKey(t *testing.T) {
-		//Given
-		var expected = []byte("1")
-		var messageBuilder = MessageBuilder{}
-		//When
-		actual := messageBuilder.WithKey(expected).key
-		//Then
-		if !bytes.Equal(expected, actual) {
-			t.Errorf("Expected: %s, Actual: %s", expected, actual)
-		}
+func Test_WithKey(t *testing.T) {
+	//Given
+	var expected = []byte("1")
+	var messageBuilder = MessageBuilder{}
+	//When
+	actual := messageBuilder.WithKey(expected).key
+	//Then
+	if !bytes.Equal(expected, actual) {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
 	}
+}
 
-	func Test_WithValue(t *testing.T) {
-		//Given
-		var expected = []byte("1")
-		var messageBuilder = MessageBuilder{}
-		//When
-		actual := messageBuilder.WithValue(expected).value
-		//Then
-		if !bytes.Equal(expected, actual) {
-			t.Errorf("Expected: %s, Actual: %s", expected, actual)
-		}
+func Test_WithValue(t *testing.T) {
+	//Given
+	var expected = []byte("1")
+	var messageBuilder = MessageBuilder{}
+	//When
+	actual := messageBuilder.WithValue(expected).value
+	//Then
+	if !bytes.Equal(expected, actual) {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
 	}
+}
 
-	func Test_WithPartition(t *testing.T) {
-		//Given
-		var expected = 1
-		var messageBuilder = MessageBuilder{}
-		//When
-		actual := messageBuilder.WithPartition(expected).partition
-		//Then
-		if *actual != expected {
-			t.Errorf("Expected: %d, Actual: %d", expected, *actual)
-		}
+func Test_WithPartition(t *testing.T) {
+	//Given
+	var expected = 1
+	var messageBuilder = MessageBuilder{}
+	//When
+	actual := messageBuilder.WithPartition(expected).partition
+	//Then
+	if *actual != expected {
+		t.Errorf("Expected: %d, Actual: %d", expected, *actual)
 	}
+}
 
-	func Test_WithHeaders(t *testing.T) {
-		//Given
-		var expected = []Header{
-			{Key: "x-retry-count", Value: []byte("1")},
-		}
-		var messageBuilder = MessageBuilder{}
-		//When
-		actual := messageBuilder.WithHeaders(expected).headers
-		//Then
-		if !bytes.Equal(actual[0].Value, expected[0].Value) {
-			t.Errorf("Expected: %s, Actual: %s", expected[0].Value, actual[0].Value)
-		}
+func Test_WithHeaders(t *testing.T) {
+	//Given
+	var expected = []Header{
+		{Key: "x-retry-count", Value: []byte("1")},
 	}
+	var messageBuilder = MessageBuilder{}
+	//When
+	actual := messageBuilder.WithHeaders(expected).headers
+	//Then
+	if !bytes.Equal(actual[0].Value, expected[0].Value) {
+		t.Errorf("Expected: %s, Actual: %s", expected[0].Value, actual[0].Value)
+	}
+}
 
-	func Test_WithHighWatermark(t *testing.T) {
-		//Given
-		var expected = int64(1)
-		var messageBuilder = MessageBuilder{}
-		//When
-		actual := messageBuilder.WithHighWatermark(expected).highWaterMark
-		//Then
-		if *actual != expected {
-			t.Errorf("Expected: %d, Actual: %d", expected, *actual)
-		}
+func Test_WithHighWatermark(t *testing.T) {
+	//Given
+	var expected = int64(1)
+	var messageBuilder = MessageBuilder{}
+	//When
+	actual := messageBuilder.WithHighWatermark(expected).highWaterMark
+	//Then
+	if *actual != expected {
+		t.Errorf("Expected: %d, Actual: %d", expected, *actual)
 	}
 }
