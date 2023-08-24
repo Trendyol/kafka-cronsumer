162b75b163dccffa7efe897f51ea069b0a5c9355
hayrienes.gokdag <hayrienes.gokdag@trendyol.com>
2023-08-24 10:21:00 +0300

    chore: add unit tests

--- pkg/kafka/cronsumer_message_test.go
+++ pkg/kafka/cronsumer_message_test.go
@@ -1,6 +1,7 @@
 package kafka
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
@@ -53,4 +54,78 @@ func Test_Should_Build_Message_With_All_Fields(t *testing.T) {
 			t.Errorf("Expected: %+v, Actual: %+v", expectedMessage, message)
 		}
 	})
+}
+
+func Test_WithTopic(t *testing.T) {
+	// Given
+	expected := "topic"
+	messageBuilder := MessageBuilder{}
+	// When
+	actual := messageBuilder.WithTopic(expected).topic
+	// Then
+	if *actual != expected {
+		t.Errorf("Expected: %s, Actual: %s", expected, *actual)
+	}
+}
+
+func Test_WithKey(t *testing.T) {
+	// Given
+	expected := []byte("1")
+	messageBuilder := MessageBuilder{}
+	// When
+	actual := messageBuilder.WithKey(expected).key
+	// Then
+	if !bytes.Equal(expected, actual) {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func Test_WithValue(t *testing.T) {
+	// Given
+	expected := []byte("1")
+	messageBuilder := MessageBuilder{}
+	// When
+	actual := messageBuilder.WithValue(expected).value
+	// Then
+	if !bytes.Equal(expected, actual) {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func Test_WithPartition(t *testing.T) {
+	// Given
+	expected := 1
+	messageBuilder := MessageBuilder{}
+	// When
+	actual := messageBuilder.WithPartition(expected).partition
+	// Then
+	if *actual != expected {
+		t.Errorf("Expected: %d, Actual: %d", expected, *actual)
+	}
+}
+
+func Test_WithHeaders(t *testing.T) {
+	// Given
+	expected := []Header{
+		{Key: "x-retry-count", Value: []byte("1")},
+	}
+	messageBuilder := MessageBuilder{}
+	// When
+	actual := messageBuilder.WithHeaders(expected).headers
+	// Then
+	if !bytes.Equal(actual[0].Value, expected[0].Value) {
+		t.Errorf("Expected: %s, Actual: %s", expected[0].Value, actual[0].Value)
+	}
+}
+
+func Test_WithHighWatermark(t *testing.T) {
+	// Given
+	expected := int64(1)
+	messageBuilder := MessageBuilder{}
+	// When
+	actual := messageBuilder.WithHighWatermark(expected).highWaterMark
+	// Then
+	if *actual != expected {
+		t.Errorf("Expected: %d, Actual: %d", expected, *actual)
+	}
 }
